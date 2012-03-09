3a55b300e5382e4e89a4112561882ba94aa3ec8a
phalphalak@gmail.com <phalphalak@gmail.com>
2012-03-09 23:03:14 +0100

    add native jogl libs

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject cartographer "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :main cartographer.core
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [
+                 [org.clojure/clojure "1.3.0"]
+                 [org.clojars.phalphalak/jogl-native-deps "2.0-rc5"]])
